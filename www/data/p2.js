98e88ebebbd93739e3fe18068045a1497d89e559
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-06-24 14:35:33 -0500

    Added to Core Concepts Q6

--- www/data/p2.js
+++ www/data/p2.js
@@ -68,7 +68,7 @@
             { "Id": 1055, "QuestionId": 1010, "Name": "A deliverable.", "IsAnswer": false },
             { "Id": 1056, "QuestionId": 1010, "Name": "The Standards Information Base.", "IsAnswer": true },
             { "Id": 1057, "QuestionId": 1010, "Name": "A building block.", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "The Governance Log.", "IsAnswer": false }]
+            { "Id": 1058, "QuestionId": 1010, "Name": "The Governance Log.", "IsAnswer": true }]
                 
        
     }
